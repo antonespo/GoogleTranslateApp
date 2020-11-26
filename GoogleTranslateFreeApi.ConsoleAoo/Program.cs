dcd2615f81511cbaf6da9d39c4bf3c105408cfe0
AntonioE <antonio.esposito4@live.it>
2020-11-26 17:18:22 +0100

    Aggiungere i file di progetto.

---
+++ GoogleTranslateFreeApi.ConsoleAoo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GoogleTranslateFreeApi.ConsoleAoo
+{
+
+    class Program
+    {
+        private static readonly GoogleTranslator Translator = new GoogleTranslator();
+        
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Please type your sentence in Italian: ");
+			var text = Console.ReadLine();
+            TranslationResult result = Translator.TranslateAsync(text, Language.Auto, Language.English).GetAwaiter().GetResult();
+            Console.WriteLine($"Sentence in Italian: {text}");
+            Console.WriteLine($"Sentence in English: {result.MergedTranslation}");
+
+        }
+    }
+
+   
+}
